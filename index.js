c488851d2f3e75f046252e3bba7fcdc2650b6adf
Seth Wheeler <wheelersc@cox.net>
2023-05-27 18:55:01 -0500

    add example usage and update gitignore

---
+++ index.js
@@ -0,0 +1,20 @@
+const musicEngine = require('./dev/MusicEngine.js');
+
+let length = 60; // 1 hour
+
+// this makes 10 wav files, all of which are 1 hour long
+
+Promise.all([
+  musicEngine('output0.wav', length),
+  musicEngine('output1.wav', length),
+  musicEngine('output2.wav', length),
+  musicEngine('output3.wav', length),
+  musicEngine('output4.wav', length),
+  musicEngine('output5.wav', length),
+  musicEngine('output6.wav', length),
+  musicEngine('output7.wav', length),
+  musicEngine('output8.wav', length),
+  musicEngine('output9.wav', length),
+]).then(() => {
+  console.log('done');
+})
